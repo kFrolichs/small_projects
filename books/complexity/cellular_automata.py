91c79fa40c571a8b889ab4072ac23e835eb07c13
kFrolichs <k.frolichs@gmail.com>
2022-05-03 23:56:54 +0200

    Added simple cellular automata

---
+++ books/complexity/cellular_automata.py
@@ -0,0 +1,54 @@
+# Some code to test out 1 dimensional cellular automata
+import numpy as np
+import matplotlib.pyplot as plt
+
+# General Settings
+radius = 3
+num_state = 2
+lattice_len = 100
+timestep = 100
+latMat = np.zeros((timestep,lattice_len))
+
+state_options = num_state**radius
+number_rules  = num_state**state_options
+
+# Choose a rule and convert to binary
+rNum = int(input(f'Please choose a rule [1-{number_rules}]: '))-1
+rule = format(rNum, '08b')
+print(f'Your chosen rule: {rule}')
+
+# Attach the rule to the states
+state_rule = {}
+for i in range(state_options):
+    state_rule[format(i,'03b')] = rule[i]
+
+# Create a random lattice (change numbers to strings)
+lat = np.random.randint(0,2,lattice_len)
+lattice = []
+for l in lat:
+    lattice.append(str(l))
+
+# Check each position of the lattice
+pos = [i for i in range(lattice_len)]
+pos.insert(0,-1)
+pos.append(0)
+
+fig, ax = plt.subplots()
+# Loop over the lattice and create a new lattice
+new_lat = ['0' for _ in range(lattice_len)]
+int_lat = [0 for _ in range(lattice_len)]
+for ts in range(timestep):
+    # Ugly solution
+    for iInt in range(lattice_len):
+        int_lat[iInt] = int(lattice[iInt])
+    latMat[ts,:] = int_lat
+
+    # print(lattice)
+    for idx in range(lattice_len):
+        state = lattice[pos[idx-1]]+lattice[pos[idx]]+lattice[pos[idx+1]]
+        new_lat[idx] = state_rule[state]
+    lattice = new_lat
+
+# Plotting the results (Need to convert the string to integers)
+ax.imshow(latMat)
+plt.show()
